5472c14af26c62236b1542d935cd7d174125b1f9
2023357 <147404883+2023357@users.noreply.github.com>
2025-11-30 21:35:43 +0000

    Create MenuOption.java
    Added MenuOption enum for menu options

---
+++ src/controller/MenuOption.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author Acer
+ */
+
+
+public enum MenuOption {
+    SORT,
+    SEARCH,
+    ADD_RECORDS,
+    BINARY_TREE,
+    EXIT
+}
